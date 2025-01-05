969425a521b0bd2a4e45e42ea2566d536c7280ec
Fano Raiza <fano@Fanos-MacBook-Pro.local>
2025-01-05 10:52:22 +0300

    Change to python pip

--- install.sh
+++ install.sh
@@ -15,13 +15,20 @@ sudo chown $USER:$USER /var/log/sms_gateway
 sudo mkdir -p /var/run/sms_gateway
 sudo chown $USER:$USER /var/run/sms_gateway
 
-# Install virtualenv if not already installed
+# Install Python 2.7 and required system packages
 sudo apt-get update
-sudo apt-get install -y python2.7 python-pip
-sudo pip install virtualenv
+sudo apt-get install -y python2.7 curl
+
+# Install pip for Python 2.7
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2.7 get-pip.py
+rm get-pip.py
+
+# Install virtualenv
+sudo python2.7 -m pip install virtualenv
 
 # Set up Python 2.7 virtual environment
-virtualenv -p python2.7 /opt/sms_gateway/venv
+python2.7 -m virtualenv /opt/sms_gateway/venv
 source /opt/sms_gateway/venv/bin/activate
 
 # Install required packages from requirements.txt
@@ -48,7 +55,9 @@ sudo systemctl start sms_gateway
 sudo usermod -a -G dialout $USER
 
 # Set proper permissions for USB device
-sudo chmod 666 /dev/ttyUSB2
+if [ -e /dev/ttyUSB2 ]; then
+    sudo chmod 666 /dev/ttyUSB2
+fi
 
 echo "Installation completed successfully!"
 echo "Note: You may need to log out and back in for the dialout group changes to take effect."
