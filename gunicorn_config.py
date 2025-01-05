562bb8cd08ca1ef857297849d7c16ffdfbc7ef25
Fano Raiza <fano@Fanos-MacBook-Pro.local>
2025-01-05 11:22:42 +0300

    change to gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,7 @@
 # gunicorn_config.py
 import multiprocessing
 import os
+import errno
 
 # Server socket
 bind = "0.0.0.0:5000"
@@ -28,9 +29,19 @@ user = None
 group = None
 tmp_upload_dir = None
 
+def makedirs_exist_ok(path):
+    """Python 2.7 compatible makedirs with exist_ok functionality."""
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 def on_starting(server):
     """
     Server hook that runs before the master process is initialized.
     """
-    os.makedirs("/var/run/sms_gateway", exist_ok=True)
-    os.makedirs("/var/log/sms_gateway", exist_ok=True)
+    makedirs_exist_ok("/var/run/sms_gateway")
+    makedirs_exist_ok("/var/log/sms_gateway")
